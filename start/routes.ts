f2efe35ec106fb551b12753daf094fcc54024a63
Sommeil1 <brendaelvira182@gmail.com>
2024-06-09 15:16:38 +0100

    for

--- start/routes.ts
+++ start/routes.ts
@@ -9,4 +9,5 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+router.on('/').render('pages/home');
+router.on('/form').render('pages/Forms/register');
